023210e28e449c2eb71fb0870628f0a375950393
Naoki0618 <nok.h0618@gmail.com>
2022-06-12 20:47:15 +0900

    update

---
+++ myapp/static/script.js
@@ -0,0 +1,17 @@
+$(function () {
+  // 検索項目のアコーディオン
+  $('.contents_list').click(function () {
+    var $answer = $('.answer');
+    if ($answer.hasClass('open')) {
+      $answer.removeClass('open');
+      // slideUpメソッドを用いて、$answerを隠してください
+      $answer.slideUp();
+
+    } else {
+      $answer.addClass('open');
+      // slideDownメソッドを用いて、$answerを表示してください
+      $answer.slideDown();
+
+    }
+  });
+});
